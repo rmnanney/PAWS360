dc416f7db2718ce8046e3697589990507a5f041f
Zenith <85917176+zeenith09@users.noreply.github.com.>
2025-09-18 22:49:47 -0500

    create login

---
+++ app/lib/placeholder-img.ts
@@ -0,0 +1,10 @@
+import data from './placeholder-img.json';
+
+export type ImagePlaceholder = {
+  id: string;
+  description: string;
+  imageUrl: string;
+  imageHint: string;
+};
+
+export const PlaceHolderImages: ImagePlaceholder[] = data.placeholderImages;
