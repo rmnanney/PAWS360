69103dae89d7f01d1c3f76107e8b01fc307ac345
Zack <152580273+ZackHawkins@users.noreply.github.com>
2025-09-21 10:41:53 -0500

    Database Update

--- src/main/java/com/uwm/paws360/Controller/UserController.java
+++ src/main/java/com/uwm/paws360/Controller/UserController.java
@@ -2,16 +2,20 @@ package com.uwm.paws360.Controller;
 
 import com.uwm.paws360.JPARepository.UserRepository;
 import com.uwm.paws360.Entity.Users;
+import com.uwm.paws360.Service.UserService;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController()
+@RequestMapping("/users")
 public class UserController {
 
     private final UserRepository repository;
+    private final UserService userService;
 
-    public UserController(UserRepository repository){
+    public UserController(UserRepository repository, UserService userService){
         this.repository = repository;
+        this.userService = userService;
     }
 
     @GetMapping("users/search/{user-id}")
@@ -29,9 +33,14 @@ public class UserController {
         return repository.findAllByFirstnameLike(name);
     }
 
-    @PostMapping("/create/user")
+    @GetMapping("users/search/{user-email}")
+    public Users findUsersByEmail(@PathVariable("user-email") String email){
+        return repository.findUsersByEmailLikeIgnoreCase(email);
+    }
+
+    @PostMapping("/create")
     public Users createUser(@RequestBody Users user){
-       return repository.save(user);
+       return userService.createUser(user);
     }
 
     @DeleteMapping("users/{user-id}")
