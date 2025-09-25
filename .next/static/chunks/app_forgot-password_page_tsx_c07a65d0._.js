fbd0b8d81838c7e65f71cf3999ba2ce0e55a4d46
Zenith <85917176+zeenith09@users.noreply.github.com>
2025-09-25 01:25:39 -0500

    this is just Next's caching

--- .next/static/chunks/app_forgot-password_page_tsx_c07a65d0._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_4666c61e._.js",
-  "static/chunks/app_forgot-password_styles_module_23e71f4b.css"
-],
-    source: "dynamic"
-});
