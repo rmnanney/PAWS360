47919f47f5f7f7ed6fddb7b0c07b3892ce88e42d
Zenith <85917176+zeenith09@users.noreply.github.com.>
2025-09-25 01:19:45 -0500

    some homepage functions + some styling

--- .next/static/chunks/app_layout_tsx_c0237562._.js
+++ .next/static/chunks/app_layout_tsx_c0237562._.js
@@ -1,9 +1,9 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/app_global_d684f999.css",
+  "static/chunks/app_795582b4._.css",
   "static/chunks/node_modules_f80c733c._.js",
-  "static/chunks/app_557ecfee._.js"
+  "static/chunks/app_d6f60d55._.js"
 ],
     source: "dynamic"
 });
